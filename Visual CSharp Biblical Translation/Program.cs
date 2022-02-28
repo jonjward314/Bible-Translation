6ac7d0a076752a6e0c2c62dc2f1de6a8fe682aa1
jonjward314 <75668950+jonjward314@users.noreply.github.com>
2022-02-28 12:38:34 -0500

    Work from 2/28/2022
    Started In On Genesis 1 verses 1 and 2.
    Concepts  explored God as class representable entity that creates the Earth and Heavens. I thought the Earth and heavens would be interestingly represented via a class Called Spacetime that had members representing the laws of the universe. Also Planets and etc could be added as data members after I initiate those classes

---
+++ Visual CSharp Biblical Translation/Program.cs
@@ -0,0 +1,28 @@
+﻿SpaceTime Earth = new SpaceTime(God.Create_SP_TM("Earth"));
+
+class God {
+    //Data Members
+    //Methods
+    public static SpaceTime Create_SP_TM(string Realm)
+    {
+        switch (Realm)
+        { //Switch Statement Refresher Courtesty of:https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/statements/selection-statements
+            case "Earth":
+                SpaceTime Earth = new SpaceTime();
+                Earth.Effulgence_State = new List<string>
+                break;
+            case "Heavens":
+
+                break;
+        }
+    }
+}
+class SpaceTime
+{
+    Effulgence_State[] 
+    //Data Members
+    //Constructors
+    //Methods
+    
+
+}
